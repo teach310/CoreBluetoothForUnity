013c021de4e52e809ac877f1318c299dc9894cf3
teach310 <okms310@yahoo.co.jp>
2023-09-06 22:41:59 +0900

    create CB4UNativeApp project

---
+++ Plugins/CoreBluetoothForUnity/CB4UNativeApp/CB4UNativeApp/CB4UNativeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CB4UNativeAppApp.swift
+//  CB4UNativeApp
+//
+//  Created by Taichi Sato on 2023/09/06.
+//
+
+import SwiftUI
+
+@main
+struct CB4UNativeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plugins/CoreBluetoothForUnity/CB4UNativeApp/CB4UNativeApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CB4UNativeApp
+//
+//  Created by Taichi Sato on 2023/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
