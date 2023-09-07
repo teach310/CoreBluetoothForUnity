ea732eddba832b7486408e60687f4c7618490b66
teach310 <okms310@yahoo.co.jp>
2023-09-07 23:26:58 +0900

    create custom package

---
+++ Packages/com.teach310.core-bluetooth-for-unity/Runtime/CoreBluetoothForUnity.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoreBluetooth
+{
+    public class CoreBluetoothForUnity
+    {
+        public void Hello()
+        {
+            Debug.Log("Hello from CoreBluetoothForUnity");
+        }
+    }
+}
