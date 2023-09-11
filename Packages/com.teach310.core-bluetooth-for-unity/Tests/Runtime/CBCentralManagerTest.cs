8d06dc6959f40505179e5f8a8c19f573e3d2a11b
teach310 <okms310@yahoo.co.jp>
2023-09-11 22:33:23 +0900

    test DidUpdateState
    CoreBluetoothではCBCentralManagerをdelegateを引数として初期化した場合、
    非同期でstateの確認を行う。
    その仕様をふまえたテスト

--- Packages/com.teach310.core-bluetooth-for-unity/Tests/Runtime/CBCentralManagerTest.cs
+++ Packages/com.teach310.core-bluetooth-for-unity/Tests/Runtime/CBCentralManagerTest.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Collections;
 using CoreBluetooth;
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace CoreBluetoothTests
 {
     public class CBCentralManagerDelegateMock : CBCentralManagerDelegate
     {
-        public void DidUpdateState(CBCentralManager central) { }
+        public CBManagerState state { get; private set; } = CBManagerState.unknown;
+
+        public void DidUpdateState(CBCentralManager central) => state = central.state;
     }
 
     public class CBCentralManagerTest
@@ -24,6 +30,27 @@ namespace CoreBluetoothTests
             using (centralManager = CBCentralManager.Create()) { }
             var delegateMock = new CBCentralManagerDelegateMock();
             Assert.That(() => centralManager.centralManagerDelegate = delegateMock, Throws.TypeOf<System.ObjectDisposedException>());
+        }
+
+        [UnityTest]
+        public IEnumerator DidUpdateState_CalledOnCreateAsync()
+        {
+            var delegateMock = new CBCentralManagerDelegateMock();
+            using var centralManager = CBCentralManager.Create(delegateMock);
+            Assert.That(delegateMock.state, Is.EqualTo(CBManagerState.unknown));
+
+            yield return WaitUntilWithTimeout(() => delegateMock.state != CBManagerState.unknown, 1f);
+            Assert.That(delegateMock.state, Is.Not.EqualTo(CBManagerState.unknown));
+        }
+
+        IEnumerator WaitUntilWithTimeout(Func<bool> predicate, float timeout)
+        {
+            var elapsedTime = 0f;
+            while (!predicate() && elapsedTime < timeout)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
