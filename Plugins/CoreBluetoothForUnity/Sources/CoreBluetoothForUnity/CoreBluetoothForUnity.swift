38b1eaee37114dab56a295e08ff86beade88b656
teach310 <okms310@yahoo.co.jp>
2023-09-11 22:24:54 +0900

    add Plugin cb4u_central_manager_register_handlers
    stateをUnity側に通知。
    stateだけを送ると、どのインスタンスが呼ばれたのかわからないため自身のポインタを引数に追加している。
    
    ## コールバックでインスタンスを特定するための選択肢
    1. Nativeのクラスのポインタをコールバックの引数にする
    2. register_handlersでコールバックを受け取るクラスを渡しておく
    3. クラスにIDを付与し、そのIDをコールバックの引数にする
    
    このうち一番1が楽に目的を達成できたため1を採用した。

--- Plugins/CoreBluetoothForUnity/Sources/CoreBluetoothForUnity/CoreBluetoothForUnity.swift
+++ Plugins/CoreBluetoothForUnity/Sources/CoreBluetoothForUnity/CoreBluetoothForUnity.swift
@@ -7,3 +7,15 @@ public func cb4u_central_manager_new() -> UnsafeMutableRawPointer {
 public func cb4u_central_manager_release(_ centralManagerPtr: UnsafeRawPointer) {
     Unmanaged<CB4UCentralManager>.fromOpaque(centralManagerPtr).release()
 }
+
+public typealias CB4UCentralManagerDidUpdateStateHandler = @convention(c) (UnsafeRawPointer, Int32) -> Void
+
+@_cdecl("cb4u_central_manager_register_handlers")
+public func cb4u_central_manager_register_handlers(
+    _ centralPtr: UnsafeRawPointer,
+    _ didUpdateStateHandler: @escaping CB4UCentralManagerDidUpdateStateHandler
+) {
+    let instance = Unmanaged<CB4UCentralManager>.fromOpaque(centralPtr).takeUnretainedValue()
+    
+    instance.didUpdateStateHandler = didUpdateStateHandler
+}
