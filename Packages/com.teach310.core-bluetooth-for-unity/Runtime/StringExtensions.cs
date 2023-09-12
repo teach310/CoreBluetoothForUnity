d589beb77f7e28b4fecefcb0fb8370b736c366f6
teach310 <okms310@yahoo.co.jp>
2023-09-13 00:22:06 +0900

    CBUUIDに変換できるかを確認する拡張メソッドを追加
    swift側でCBUUID(string:)に無効な文字列を渡すとクラッシュしてしまうため事前に防ぐ

---
+++ Packages/com.teach310.core-bluetooth-for-unity/Runtime/StringExtensions.cs
@@ -0,0 +1,14 @@
+using System.Text.RegularExpressions;
+
+namespace CoreBluetooth
+{
+    public static class StringExtensions
+    {
+        private static readonly Regex CBUUIDRegex = new Regex(@"^([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}|[0-9a-f]{8}|[0-9a-f]{4})$", RegexOptions.IgnoreCase);
+
+        public static bool IsCBUUID(this string uuidString)
+        {
+            return CBUUIDRegex.IsMatch(uuidString);
+        }
+    }
+}
