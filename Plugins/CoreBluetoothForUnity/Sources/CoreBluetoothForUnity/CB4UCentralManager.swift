3b5519b78a941aa4882cef1ac56cd6b41b56a032
teach310 <okms310@yahoo.co.jp>
2023-09-14 21:49:24 +0900

    add Plugin scan関連メソッド、イベント

--- Plugins/CoreBluetoothForUnity/Sources/CoreBluetoothForUnity/CB4UCentralManager.swift
+++ Plugins/CoreBluetoothForUnity/Sources/CoreBluetoothForUnity/CB4UCentralManager.swift
@@ -5,7 +5,8 @@ import CoreBluetooth
 public class CB4UCentralManager : NSObject {
     private var centralManager: CBCentralManager!
     
-    var didUpdateStateHandler: CB4UCentralManagerDidUpdateStateHandler?
+    public var didDiscoverPeripheralHandler: CB4UCentralManagerDidDiscoverPeripheralHandler?
+    public var didUpdateStateHandler: CB4UCentralManagerDidUpdateStateHandler?
     
     public override init() {
         super.init()
@@ -16,12 +17,33 @@ public class CB4UCentralManager : NSObject {
     func selfPointer() -> UnsafeMutableRawPointer {
         return Unmanaged.passUnretained(self).toOpaque()
     }
+    
+    public func scanForPeripherals(withServices serviceUUIDs: [CBUUID]?) {
+        centralManager.scanForPeripherals(withServices: serviceUUIDs)
+    }
+    
+    public func stopScan() {
+        centralManager.stopScan()
+    }
+    
+    public var isScanning: Bool {
+        return centralManager.isScanning
+    }
 }
 
 extension CB4UCentralManager : CBCentralManagerDelegate {
     
     // MARK: - CBCentralManagerDelegate
     
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {        
+        let peripheralId = peripheral.identifier.uuidString
+        peripheralId.withCString { (uuidCString) in
+            (peripheral.name ?? "").withCString { (nameCString) in
+                didDiscoverPeripheralHandler?(selfPointer(), uuidCString, nameCString, Int32(RSSI.intValue))
+            }
+        }
+    }
+
     public func centralManagerDidUpdateState(_ central: CBCentralManager) {
         didUpdateStateHandler?(selfPointer(), Int32(central.state.rawValue))
     }
