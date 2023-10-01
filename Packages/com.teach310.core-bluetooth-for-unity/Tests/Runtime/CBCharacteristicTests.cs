36e75d3d48978123745f734f38af4a093e6ec88a
teach310 <okms310@yahoo.co.jp>
2023-10-01 21:21:13 +0900

    change: CBCharacteristic properties to virtual
    CBMutableCharacteristicでoverrideするため。
    
    参考
    XamarinのCBCharacteristic.Value
    https://learn.microsoft.com/en-us/dotnet/api/corebluetooth.cbcharacteristic.value?view=xamarin-ios-sdk-12
    
    overrideではなく、newキーワードで上書きすることも可能だと思うが、CBCharacteristic側で上書きされることを想定したコードになっていたほうが綺麗に感じた。

---
+++ Packages/com.teach310.core-bluetooth-for-unity/Tests/Runtime/CBCharacteristicTests.cs
@@ -0,0 +1,23 @@
+using System;
+using CoreBluetooth;
+using NUnit.Framework;
+
+namespace CoreBluetoothTests
+{
+    public class CBCharacteristicTests : CBTests
+    {
+        [Test]
+        public void Value_Set_NotImplemented()
+        {
+            var characteristic = new CBCharacteristic(validUUID1, null, null);
+            Assert.That(() => characteristic.Value = new byte[] { 0x01 }, Throws.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void Properties_Set_NotImplemented()
+        {
+            var characteristic = new CBCharacteristic(validUUID1, null, null);
+            Assert.That(() => characteristic.Properties = CBCharacteristicProperties.Broadcast, Throws.TypeOf<NotImplementedException>());
+        }
+    }
+}
