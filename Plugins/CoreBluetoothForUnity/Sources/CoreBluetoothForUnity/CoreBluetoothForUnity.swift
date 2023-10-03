89ed74c0bbb182fc309c7ce573df6cd3f9aeaebc
teach310 <okms310@yahoo.co.jp>
2023-10-03 22:45:38 +0900

    add plugin cbru_mutable_service clear, add characteristic

--- Plugins/CoreBluetoothForUnity/Sources/CoreBluetoothForUnity/CoreBluetoothForUnity.swift
+++ Plugins/CoreBluetoothForUnity/Sources/CoreBluetoothForUnity/CoreBluetoothForUnity.swift
@@ -218,6 +218,21 @@ public func cb4u_mutable_service_release(_ servicePtr: UnsafeRawPointer) {
     Unmanaged<CB4UMutableService>.fromOpaque(servicePtr).release()
 }
 
+@_cdecl("cb4u_mutable_service_clear_characteristics")
+public func cb4u_mutable_service_clear_characteristics(_ servicePtr: UnsafeRawPointer) {
+    let service = Unmanaged<CB4UMutableService>.fromOpaque(servicePtr).takeUnretainedValue()
+    
+    service.clearCharacteristics()
+}
+
+@_cdecl("cb4u_mutable_service_add_characteristic")
+public func cb4u_mutable_service_add_characteristic(_ servicePtr: UnsafeRawPointer, _ characteristicPtr: UnsafeRawPointer) {
+    let service = Unmanaged<CB4UMutableService>.fromOpaque(servicePtr).takeUnretainedValue()
+    let characteristic = Unmanaged<CB4UMutableCharacteristic>.fromOpaque(characteristicPtr).takeUnretainedValue()
+    
+    service.addCharacteristic(characteristic)
+}
+
 @_cdecl("cb4u_mutable_characteristic_new")
 public func cb4u_mutable_characteristic_new(
     _ characteristicUUID: UnsafePointer<CChar>,
