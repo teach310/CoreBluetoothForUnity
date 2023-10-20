bf775a1b06402e0bdd86a546f19d31187888cef8
teach310 <okms310@yahoo.co.jp>
2023-10-20 23:30:15 +0900

    test Remove, RemoveAllServices
    サービス取得できないため、クラッシュしないことを確認

--- Packages/com.teach310.core-bluetooth-for-unity/Tests/Runtime/CBPeripheralManagerTests.cs
+++ Packages/com.teach310.core-bluetooth-for-unity/Tests/Runtime/CBPeripheralManagerTests.cs
@@ -73,6 +73,46 @@ namespace CoreBluetoothTests
         }
 
         [UnityTest]
+        public IEnumerator RemoveService()
+        {
+            var delegateMock = new CBPeripheralManagerDelegateMock();
+            using var peripheralManager = new CBPeripheralManager(delegateMock);
+
+            yield return WaitUntilWithTimeout(() => delegateMock.State != CBManagerState.Unknown, 1f);
+            if (delegateMock.State != CBManagerState.PoweredOn) yield break;
+
+            using var service = new CBMutableService(validUUID1, true);
+            using var characteristic1 = new CBMutableCharacteristic(validUUID2, CBCharacteristicProperties.Read, null, CBAttributePermissions.Readable);
+            using var characteristic2 = new CBMutableCharacteristic(validUUID3, CBCharacteristicProperties.Write, null, CBAttributePermissions.Writeable);
+            var characteristics = new CBMutableCharacteristic[] { characteristic1, characteristic2 };
+            service.Characteristics = characteristics;
+            peripheralManager.AddService(service);
+
+            yield return WaitUntilWithTimeout(() => delegateMock.AddedService != null, 1f);
+            peripheralManager.RemoveService(service);
+        }
+
+        [UnityTest]
+        public IEnumerator RemoveAllServices()
+        {
+            var delegateMock = new CBPeripheralManagerDelegateMock();
+            using var peripheralManager = new CBPeripheralManager(delegateMock);
+
+            yield return WaitUntilWithTimeout(() => delegateMock.State != CBManagerState.Unknown, 1f);
+            if (delegateMock.State != CBManagerState.PoweredOn) yield break;
+
+            using var service = new CBMutableService(validUUID1, true);
+            using var characteristic1 = new CBMutableCharacteristic(validUUID2, CBCharacteristicProperties.Read, null, CBAttributePermissions.Readable);
+            using var characteristic2 = new CBMutableCharacteristic(validUUID3, CBCharacteristicProperties.Write, null, CBAttributePermissions.Writeable);
+            var characteristics = new CBMutableCharacteristic[] { characteristic1, characteristic2 };
+            service.Characteristics = characteristics;
+            peripheralManager.AddService(service);
+
+            yield return WaitUntilWithTimeout(() => delegateMock.AddedService != null, 1f);
+            peripheralManager.RemoveAllServices();
+        }
+
+        [UnityTest]
         public IEnumerator Advertising()
         {
             var delegateMock = new CBPeripheralManagerDelegateMock();
