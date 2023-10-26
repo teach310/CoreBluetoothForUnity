d73651103c160a5efa8095fbead02f2a5f516b6c
teach310 <okms310@yahoo.co.jp>
2023-10-26 23:29:51 +0900

    internalなクラスの公開用メソッドはpublicにする

--- Packages/com.teach310.core-bluetooth-for-unity/Runtime/NativeCentralManagerProxy.cs
+++ Packages/com.teach310.core-bluetooth-for-unity/Runtime/NativeCentralManagerProxy.cs
@@ -6,23 +6,23 @@ namespace CoreBluetooth
     {
         readonly SafeNativeCentralManagerHandle _handle;
 
-        internal NativeCentralManagerProxy(SafeNativeCentralManagerHandle handle, INativeCentralManagerDelegate centralManagerDelegate)
+        public NativeCentralManagerProxy(SafeNativeCentralManagerHandle handle, INativeCentralManagerDelegate centralManagerDelegate)
         {
             _handle = handle;
             _handle.SetDelegate(centralManagerDelegate);
         }
 
-        internal void Connect(CBPeripheral peripheral)
+        public void Connect(CBPeripheral peripheral)
         {
             NativeMethods.cb4u_central_manager_connect(_handle, peripheral.Handle);
         }
 
-        internal void CancelPeripheralConnection(CBPeripheral peripheral)
+        public void CancelPeripheralConnection(CBPeripheral peripheral)
         {
             NativeMethods.cb4u_central_manager_cancel_peripheral_connection(_handle, peripheral.Handle);
         }
 
-        internal void ScanForPeripherals(string[] serviceUUIDs = null)
+        public void ScanForPeripherals(string[] serviceUUIDs = null)
         {
             foreach (string uuidString in serviceUUIDs)
             {
@@ -36,12 +36,12 @@ namespace CoreBluetooth
             );
         }
 
-        internal void StopScan()
+        public void StopScan()
         {
             NativeMethods.cb4u_central_manager_stop_scan(_handle);
         }
 
-        internal bool IsScanning()
+        public bool IsScanning()
         {
             return NativeMethods.cb4u_central_manager_is_scanning(_handle);
         }
