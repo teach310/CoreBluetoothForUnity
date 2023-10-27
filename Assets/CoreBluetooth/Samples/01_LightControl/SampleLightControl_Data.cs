2e2d3a526aff6f5bf513178a84f99d836892ec3c
teach310 <okms310@yahoo.co.jp>
2023-10-27 22:51:22 +0900

    add SampleLightControl UUID

---
+++ Assets/CoreBluetooth/Samples/01_LightControl/SampleLightControl_Data.cs
@@ -0,0 +1,10 @@
+namespace CoreBluetoothSample
+{
+    public static class SampleLightControl_Data
+    {
+        // See the following for generating UUIDs:
+        // https://www.uuidgenerator.net/
+        public static readonly string ServiceUUID = "068C47B7-FC04-4D47-975A-7952BE1A576F";
+        public static readonly string CharacteristicUUID = "068C47B7-FC04-4D47-975A-7952BE1A576F";
+    }
+}
