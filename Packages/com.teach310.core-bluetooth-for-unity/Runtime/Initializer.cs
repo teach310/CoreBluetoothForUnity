cbb4d3d55f36d1efaec3f5dc0140bb3348c6fbef
teach310 <okms310@yahoo.co.jp>
2023-11-05 14:15:03 +0900

    fix Initializer
    OSX以外のUnityEditorでInitializeの中身が機能しないようにする

--- Packages/com.teach310.core-bluetooth-for-unity/Runtime/Initializer.cs
+++ Packages/com.teach310.core-bluetooth-for-unity/Runtime/Initializer.cs
@@ -7,9 +7,11 @@ namespace CoreBluetooth
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void Initialize()
         {
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX || UNITY_IOS
             ServiceLocator.Instance.Register<INativeCentralManagerCallbacks>(new NativeCentralManagerCallbacks());
             ServiceLocator.Instance.Register<INativePeripheralManagerCallbacks>(new NativePeripheralManagerCallbacks());
             ServiceLocator.Instance.Register<INativePeripheralCallbacks>(new NativePeripheralCallbacks());
+#endif
         }
     }
 }
